913c3e4d27e462880b82de43737440de1f74bf9f
thiago-coucello <thiago.coucello@gmail.com>
2023-05-04 10:51:44 -0400

    corrigindo resultados para subconjuntos 15 e 20 e alterando script de geração de resultados

--- CODIGOS/gerarResultados.py
+++ CODIGOS/gerarResultados.py
@@ -2,7 +2,9 @@ import os
 import pandas as pd
 import numpy as np
 
-RESULTS_PATH = "C:\\PIBIC\\2022-2023\\Results"
+DRIVE = os.path.splitdrive(os.getcwd())[0]
+
+RESULTS_PATH = f"{DRIVE}\\PIBIC\\2022-2023\\Results"
 OUTPUT_NAME = "results"
 OUTPUT_EXTENSION = "csv"
 
@@ -26,7 +28,6 @@ subsets = os.listdir(RESULTS_PATH)
 
 for subset in subsets: # 05 - 100
     if subset.endswith(".csv"):
-        os.remove(os.path.join(RESULTS_PATH, subset))
         continue
 
     SUBSET_RESULTS = os.path.join(RESULTS_PATH, subset)
@@ -41,6 +42,11 @@ for subset in subsets: # 05 - 100
             output_file = os.path.join(RESULTS_PATH, f"{OUTPUT_NAME}_{subset}.{OUTPUT_EXTENSION}")
             
             df = pd.read_csv(csv_path)
+
+            if df["folder"].size < 100:
+                # Não processou direito o subconjunto
+                continue
+
             result_df = pd.DataFrame()
             runtime = df["runtime"].cumsum().iloc[-1]
             val_runtime = df["val_runtime"].cumsum().iloc[-1]
@@ -48,10 +54,25 @@ for subset in subsets: # 05 - 100
             result_data = {"subset": subset, "strategy": strategy, "model": model, "runtime": runtime, "val_runtime": val_runtime, "total_runtime": total_runtime}
             for column in COLUMNS:
                 first_quartile, third_quartile = df[column].quantile([0.25, 0.75])
-                column_mean = df[column].mean()
-                result_data[f"{column}_mean"] = column_mean
-                result_data[f"{column}_lower"] = first_quartile
-                result_data[f"{column}_upper"] = third_quartile
-            result_df: pd.DataFrame = result_df.append(result_data, ignore_index = True)
+                iqr = third_quartile - first_quartile
+                column_median = df[column].median()
+                lower_whisker = 100 * (first_quartile - 1.5 * iqr)
+                upper_whisker = 100 * (third_quartile + 1.5 * iqr)
+
+                result_data[f"{column}_median"] = 100 * column_median
+                result_data[f"{column}_lower"] = 100 * first_quartile
+                result_data[f"{column}_upper"] = 100 * third_quartile
+                result_data[f"{column}_lower_whisker"] = 0 if lower_whisker < 0 else lower_whisker
+                result_data[f"{column}_upper_whisker"] = 100 if upper_whisker > 100 else upper_whisker
+                
+                # Formatando para gráfico de linhas
+                coordinates = ""
+                for (epoch, value) in zip(df["folder"], df[column]):
+                    coordinates += f"({epoch}, {100 * value})"
+                result_data[f"{column}_coords"] = coordinates
+
+            result_temp_df = pd.DataFrame([result_data])
+            result_df = pd.concat([result_df, result_temp_df], ignore_index = True)
+            #result_df: pd.DataFrame = result_df.append(result_data, ignore_index = True)
             result_df.to_csv(output_file, index=None)
             
